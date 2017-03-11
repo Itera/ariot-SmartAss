4eb5c6df2322b0b7185925466c585debb984d207
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-03-11 11:24:58 +0100

    test cam

--- devices/camera/camera-capture.py
+++ devices/camera/camera-capture.py
@@ -3,17 +3,29 @@
 from picamera import PiCamera
 import numpy as np
 from time import sleep
+from PIL import Image
 
 camera = PiCamera()
+image = "/home/pi/ariot-SmartAss/devices/camera/assets/image.jpg"
 
-def capture_image():
+def capture_image(img):
     camera.start_preview()
-    camera.resolution = (320, 240)
-    camera.framerate = 24
     sleep(2)
-    output = np.empty((240, 320, 3), dtype=np.uint8)
-    camera.capture(output, 'rgb')
+    camera.capture(img)
+    im = Image.open(img)
     camera.stop_preview()
-    return output
+    return im
 
-return capture_image()
+return capture_image(image)
+
+# def capture_image():
+#     camera.start_preview()
+#     camera.resolution = (320, 240)
+#     camera.framerate = 24
+#     sleep(2)
+#     output = np.empty((240, 320, 3), dtype=np.uint8)
+#     camera.capture(output, 'rgb')
+#     camera.stop_preview()
+#     return output
+
+# return capture_image()
