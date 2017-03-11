b8be312698c279f6fb88892f7e631bb5d6196f8d
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-03-11 10:55:07 +0100

    added script for cam

---
+++ devices/camera/camera-capture.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from picamera import PiCamera
+import numpy as np
+from time import sleep
+
+camera = PiCamera()
+
+def capture_image(img):
+    camera.start_preview()
+    camera.resolution = (320, 240)
+    camera.framerate = 24
+    sleep(2)
+    output = np.empty((240, 320, 3), dtype=np.uint8)
+    camera.capture(output, 'rgb')
+    camera.stop_preview()
+    return output
