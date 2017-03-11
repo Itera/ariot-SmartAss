44d60a8dad12bcd9870d24e3aeb5a8c2cc232c8f
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-03-11 12:21:00 +0100

    test cam

--- devices/camera/camera-capture-file.py
+++ devices/camera/camera-capture-file.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
 
 from picamera import PiCamera
-from time import sleep
 
 camera = PiCamera()
 image = "/home/pi/ariot-SmartAss/devices/camera/assets/image.jpg"
-
-def capture_image(img):
-    camera.start_preview()
-    sleep(2)
-    camera.capture(img)
-    camera.stop_preview()
-
-capture_image(image)
+camera.capture(image)
