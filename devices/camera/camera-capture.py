5f6884c3bc7f660da7e7b71a70763817c27a36a6
Joakim Myrvoll Johansen <joakimmyrvoll@gmail.com>
2017-03-11 14:54:03 +0100

    added extra task

--- devices/camera/camera-capture.py
+++ devices/camera/camera-capture.py
@@ -1,57 +1,19 @@
-from microbit import *
-from radio import *
-from random import*
+#!/usr/bin/python
 
-#turn radio on and 
-on()
-config(group=6, length=251)
+from picamera import PiCamera
 
-# 0=stein, 1=saks, 2=papir
-def win(me, he):
-    if(me == 0 and he == 1):
-        return true
-    elif(me == 0 and he == 2):
-        return false
-    elif(me == 1 and he == 0):
-        return false
-    elif(me == 1 and he == 2):
-        return true
-    elif(me == 2 and he == 0):
-        return true
-    elif(me == 2 and he == 1):
-        return false
-    else:
-        return false
-        
+resolutionX = 320
+resolutionY = 240
 
-def game():
-    attack = random.randint(0, 2)
-    send(str(attack))
-    counter = receive()
-    victory = win(attack, counter)
-    
-    if(victory):
-        display.scroll("win")
-    else:
-        display.scroll("lose")
-    display.clear()
-    
-while True:
-    if button_a.was_pressed():
-        #send the images
-        send("A")
-     
-    if accelerometer.was_gesture('shake'):
-        game()
-     
-    #receive data    
-    data = receive()
-    
-    #check that we have received any data
-    if data != None:
-        
-        #split the data into different pictures by the marker
-        display.scroll(data)
+# buffer
+filePath = "/home/pi/ariot-SmartAss/devices/camera/assets/image.jpg"
+fileType = "jpeg"
+picfile = open(filePath, 'wb')
 
-        #clear the screen
-        display.clear()
+# take the photo
+camera = PiCamera()
+camera.resolution = (resolutionX, resolutionY)
+camera.capture(picfile, fileType)
+
+# flush the buffer
+picfile.close()
